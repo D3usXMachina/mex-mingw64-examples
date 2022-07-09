c94a43221fb57b3af278392cc0feabd2381c2fdb
Joel Fischer <D3usXMachina@users.noreply.github.com>
2022-07-09 16:17:22 +0200

    Add Fortran C-mex implementation of timestwo

---
+++ c-f90-mex/test_timestwo.c
@@ -0,0 +1,30 @@
+// Copyright (c) 2022 Joel Fischer
+
+#include <stdio.h>
+
+extern void timestwo(double input, double* output);
+
+int main( int argc, char* argv[] ){
+
+    double input, output;
+    int status;
+
+    if ( argc > 1 ) {
+        status = sscanf(argv[1], "%lg", &input);
+        if ( !status ){
+            fprintf(stderr, " Invalid input argument. First input argument must be a floating point number.");
+            return 1;
+        } else {
+            printf(" Input argument: %g\n", input);
+        }
+    } else {
+        printf(" No input arguments. Using example: input = 1.3\n");
+        input = 1.3;
+    }
+
+    timestwo(input, &output);
+
+    printf("Result: %g", output);
+
+    return 0;
+}
